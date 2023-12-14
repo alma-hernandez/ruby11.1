f7217c0749e20c8907213b7b42d4b6e2cb409969
alma-hernandez <1984ahern@gmail.com>
2023-12-14 13:36:34 -0600

    merged fixes to this repo

--- bewd-todolist-user-auth/app/models/user.rb
+++ bewd-todolist-user-auth/app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
 
   validates :username, presence: true, length: { minimum: 3, maximum: 64 }
   validates :password, presence: true, length: { minimum: 8, maximum: 64 }
+  validates_uniqienes_of :username
 end
