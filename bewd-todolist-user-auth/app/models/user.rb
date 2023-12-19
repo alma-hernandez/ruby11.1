6d417c1edeea833d97c0bce25ea4bd3b81ebd3fe
alma-hernandez <1984ahern@gmail.com>
2023-12-19 10:41:21 -0600

    with encryption

--- bewd-todolist-user-auth/app/models/user.rb
+++ bewd-todolist-user-auth/app/models/user.rb
@@ -3,6 +3,17 @@ class User < ApplicationRecord
   has_many :tasks
 
   validates :username, presence: true, length: { minimum: 3, maximum: 64 }
+
   validates :password, presence: true, length: { minimum: 8, maximum: 64 }
+    before_create :hash_password
+
+    private
+
+  def hash_password
+    self.password = 
+    BCrypt::Password.create(self.password)
+  end
+
+
   validates_uniqueness_of :username
 end
